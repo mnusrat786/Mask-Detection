2a168868f8ed5c03956777e7577e5113ca6e90d2
Muhammad Osama Nusrat <osamanusrat786@gmail.com>
2021-10-18 16:15:52 +0500

    initial commit

---
+++ resizeee.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+
+arr = os.listdir("images/")
+
+for aa in arr:
+    aaa = aa.split('.')[0]
+    img = cv2.imread(f"images/{aa}")
+    ress = cv2.resize(img,(640,640))
+    cv2.imwrite(f"folder2/{aaa}.png",ress) 
